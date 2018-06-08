e13e9e666275fb6500d451f8285ef629c03f8bf1
dinesh <dinesh_soundar@yahoo.com>
2018-06-08 18:43:43 -0400

    summary
    2

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,8 +1,21 @@
+
+import './polyfills';
+
+import 'hammerjs';
+import 'web-animations-js';
+
+import {CdkTableModule} from '@angular/cdk/table';
+import {HttpClientModule} from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { UserLoginComponent } from './user-login/user-login.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
@@ -11,9 +24,100 @@ import { NotificationMessageComponent } from './notification-message/notificatio
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { UserFormComponent } from './user-form/user-form.component';
 import { SsrPageComponent } from './ssr-page/ssr-page.component';
+import {
+  MatAutocompleteModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+
+} from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  exports: [
+    CdkTableModule,
+    MatAutocompleteModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+  ]
+})
+export class DemoMaterialModule {}
+
+@NgModule({
+  imports: [BrowserModule,
+  BrowserAnimationsModule,
+  CommonModule,
+
+  FormsModule,
+  ReactiveFormsModule,
+  RouterModule,
+  BrowserAnimationsModule,
+  HttpClientModule,
+  DemoMaterialModule,
+  MatNativeDateModule,
+  ReactiveFormsModule,
+  AngularFirestoreModule.enablePersistence(),
+],
   declarations: [
     UserLoginComponent,
     HomePageComponent,
@@ -33,3 +137,22 @@ import { SsrPageComponent } from './ssr-page/ssr-page.component';
   ]
 })
 export class UiModule {}
+
+
+
+
+
+
+
+
+
+
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
+
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule} from '@angular/forms';
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -43,6 +43,8 @@
 
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
+
+import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 
 
@@ -73,6 +75,10 @@ import 'core-js/es7/reflect';
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
+
+
+import 'hammerjs';
+import 'web-animations-js';
 
 
 /***************************************************************************************************
